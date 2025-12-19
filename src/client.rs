04ea46f2e76efa5e9d00726261bb913a4127ca1c
Jusong Yu <jusong.d.yu@gmail.com>
2025-12-19 14:41:40 +0100

    dataset service protobuf

--- src/client.rs
+++ src/client.rs
@@ -1,19 +1,25 @@
 pub mod req_packager {
-    tonic::include_proto!("req_packager");
+    tonic::include_proto!("req_packager.v1");
 }
 
-use req_packager::{greeter_client::GreeterClient, HelloRequest};
+use req_packager::{dataset_service_client::DatasetServiceClient, BrowseDatasetRequest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    let mut client = DatasetServiceClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
+    // made up repo url and dataset id, should be mocked for test
+    let datarepo_url = "http://onedata.com".to_string();
+    let dataset_id = "xxx-pid".to_string();
+    let request = tonic::Request::new(BrowseDatasetRequest {
+        datarepo_url,
+        dataset_id,
     });
-    let response = client.say_hello(request).await?;
 
-    println!("RESPONSE={response:?}");
+    let mut stream = client.browse_dataset(request).await?.into_inner();
+    while let Some(resp) = stream.message().await? {
+        println!("resp: {:?}", resp);
+    }
 
     Ok(())
 }
